d4dec09a9ab9a890c5b2cb51730943fa9a61a959
mia83 <79408337+mia83@users.noreply.github.com>
2023-10-26 23:50:12 +0200

    LIB-38 Registered application 'list'

--- Django_server/HomeLibraryApp/HomeLibraryApp/urls.py
+++ Django_server/HomeLibraryApp/HomeLibraryApp/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
+# Используйте static() чтобы добавить соотношения для статических файлов
+# Только на период разработки
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('list/', include('list.urls')),
+    path('', RedirectView.as_view(url='/list/', permanent=True)),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
