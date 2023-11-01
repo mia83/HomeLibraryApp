e436ce79e04a5325821b529c9cdfd9d3556de81b
mia83 <79408337+mia83@users.noreply.github.com>
2023-11-01 21:09:43 +0100

    LIB-50 Verified data types in initial sql script

--- Docker_container/Library/docker-entrypoint-initdb.d/S20createTables.sql
+++ Docker_container/Library/docker-entrypoint-initdb.d/S20createTables.sql
@@ -1,18 +1,18 @@
 CREATE TABLE IF NOT EXISTS library.list_authors
-    (id smallint unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    last_name char(20) NOT NULL,
-    first_name char(20) NOT NULL);
+    (id bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    last_name varchar(50) NOT NULL,
+    first_name varchar(50) NOT NULL);
 CREATE TABLE IF NOT EXISTS library.list_genres
-    (id smallint unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title char(20) NOT NULL);
+    (id bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title varchar(50) NOT NULL);
 CREATE TABLE IF NOT EXISTS library.list_grades
-    (id tinyint NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    grade tinyint,
+    (id bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    grade varchar(1) NOT NULL,
     emoji blob);
 CREATE TABLE IF NOT EXISTS library.list_books
-    (id smallint unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title char(50) NOT NULL,
-    author_id smallint unsigned NOT NULL,
-    grade_id tinyint,
-    genre_id smallint unsigned,
-    comment char(255));
+    (id bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title varchar(50) NOT NULL,
+    author_id bigint(20) NOT NULL,
+    grade_id bigint(20),
+    genre_id bigint(20),
+    comment varchar(255));
