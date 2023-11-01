6ff95ea0dfff2f52d3cf7dc57b854b59429c1436
mia83 <79408337+mia83@users.noreply.github.com>
2023-11-01 16:50:26 +0100

    LIB-50 Corrected tables names and columns names in initial scripts

--- Docker_container/Library/docker-entrypoint-initdb.d/S20createTables.sql
+++ Docker_container/Library/docker-entrypoint-initdb.d/S20createTables.sql
@@ -1,17 +1,18 @@
-CREATE TABLE IF NOT EXISTS library.authors
-    (author_id smallint unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS library.list_authors
+    (id smallint unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
     last_name char(20) NOT NULL,
     first_name char(20) NOT NULL);
-CREATE TABLE IF NOT EXISTS library.genres
-    (genre_id smallint unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS library.list_genres
+    (id smallint unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title char(20) NOT NULL);
-CREATE TABLE IF NOT EXISTS library.grades
-    (grade tinyint PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS library.list_grades
+    (id tinyint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    grade tinyint,
     emoji blob);
-CREATE TABLE IF NOT EXISTS library.books
-    (book_id smallint unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS library.list_books
+    (id smallint unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title char(50) NOT NULL,
     author_id smallint unsigned NOT NULL,
-    grade tinyint,
+    grade_id tinyint,
     genre_id smallint unsigned,
     comment char(255));
--- Docker_container/Library/docker-entrypoint-initdb.d/S40fillTables.sql
+++ Docker_container/Library/docker-entrypoint-initdb.d/S40fillTables.sql
@@ -1,17 +1,17 @@
-insert into library.genres (title)
+insert into library.list_genres (title)
     values 
         ('детектив'),
         ('приключения'),
         ('фантастика'),
         ('фэнтези');
-insert into library.authors (last_name, first_name)
+insert into library.list_authors (last_name, first_name)
     values
         ('шойнеманн','фрауке'),
         ('вебб','холли'),
         ('милфорд', 'кейт'),
         ('линдгрен','астрид '),
         ('борисова','ариадна');
-insert into library.grades
+insert into library.list_grades (grade, emoji)
     values
         (1,NULL),
         (2,NULL),
@@ -19,29 +19,29 @@ insert into library.grades
         (4,NULL),
         (5,NULL);
 # -----
-# Insert into library.books
+# Insert into library.list_books
 # -----
-insert into library.books (title, author_id, grade, genre_id, comment)
-    select 'приключения кота-детектива', author_id, 5, genre_id, 'самая крутая книга в этой серии - уинстон берегись'
-    from authors a, genres g
-    where a.last_name='шойнеманн' and a.first_name = 'фрауке' and g.title='детектив';
-insert into library.books (title, author_id, grade, genre_id, comment)
-    select 'дом из зелёного стекла', author_id, 5, genre_id, 'невероятно захватывающая концовка'
-    from authors a, genres g
-    where a.last_name='милфорд' and a.first_name = 'кейт' and g.title='фэнтези';
-insert into library.books (title, author_id, grade, genre_id, comment)
-    select 'рони, дочь разбойника', author_id, 5, genre_id, 'по мотивам этой книги сняли мультсериал'
-    from authors a, genres g
-    where a.last_name='линдгрен' and a.first_name = 'астрид' and g.title='приключения';
-insert into library.books (title, author_id, grade, genre_id, comment)
-    select 'котёнок рози или острый нюх', author_id, 4, genre_id, 'начиная с середины книги становится интересно'
-    from authors a, genres g
-    where a.last_name='вебб' and a.first_name = 'холли' and g.title='приключения';
-insert into library.books (title, author_id, grade, genre_id, comment)
-    select 'записки для моих потомков', author_id, 5, genre_id, 'очень смешная книга'
-    from authors a, genres g
-    where a.last_name='борисова' and a.first_name = 'ариадна' and g.title='приключения';
-insert into library.books (title, author_id, grade, genre_id, comment)
-    select 'записки для моих потомков 2', author_id, 4, genre_id, 'первая часть понравилась больше'
-    from authors a, genres g
-    where a.last_name='борисова' and a.first_name = 'ариадна' and g.title='приключения';
+insert into library.list_books (title, author_id, grade_id, genre_id, comment)
+    select 'приключения кота-детектива', a.id, gr.id, ge.id, 'самая крутая книга в этой серии - уинстон берегись'
+    from list_authors a, list_genres ge, list_grades gr
+    where a.last_name='шойнеманн' and a.first_name='фрауке' and ge.title='детектив' and gr.grade=5;
+insert into library.list_books (title, author_id, grade_id, genre_id, comment)
+    select 'дом из зелёного стекла', a.id, gr.id, ge.id, 'невероятно захватывающая концовка'
+    from list_authors a, list_genres ge, list_grades gr
+    where a.last_name='милфорд' and a.first_name = 'кейт' and ge.title='фэнтези' and gr.grade=5;
+insert into library.list_books (title, author_id, grade_id, genre_id, comment)
+    select 'рони, дочь разбойника', a.id, gr.id, ge.id, 'по мотивам этой книги сняли мультсериал'
+    from list_authors a, list_genres ge, list_grades gr
+    where a.last_name='линдгрен' and a.first_name = 'астрид' and ge.title='приключения' and gr.grade=5;
+insert into library.list_books (title, author_id, grade_id, genre_id, comment)
+    select 'котёнок рози или острый нюх', a.id, gr.id, ge.id, 'начиная с середины книги становится интересно'
+    from list_authors a, list_genres ge, list_grades gr
+    where a.last_name='вебб' and a.first_name = 'холли' and ge.title='приключения' and gr.grade=4;
+insert into library.list_books (title, author_id, grade_id, genre_id, comment)
+    select 'записки для моих потомков', a.id, gr.id, ge.id, 'очень смешная книга'
+    from list_authors a, list_genres ge, list_grades gr
+    where a.last_name='борисова' and a.first_name = 'ариадна' and ge.title='приключения' and gr.grade=5;
+insert into library.list_books (title, author_id, grade_id, genre_id, comment)
+    select 'записки для моих потомков 2', a.id, gr.id, ge.id, 'первая часть понравилась больше'
+    from list_authors a, list_genres ge, list_grades gr
+    where a.last_name='борисова' and a.first_name = 'ариадна' and ge.title='приключения' and gr.grade=4;
