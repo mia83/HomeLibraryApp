b0403b524e6efe5901e288367e8fe5000ab33556
xakepKot <sashamochalin2007@gmail.com>
2023-11-08 16:54:02 +0100

    LIB-43 html page

--- Django_server/HomeLibraryApp/list/views.py
+++ Django_server/HomeLibraryApp/list/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import BookForm
 
-# Create your views here.
+def add_new_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    data = {
+        'form': form
+    }
+
+    return render(request, 'list/add_new_book.html', data)
