7a1d89ac33d973b4b176f7c37befc214f26f484f
mia83 <79408337+mia83@users.noreply.github.com>
2023-11-23 22:12:58 +0100

    LIB-71 Infrastructure code moved to another repository

--- InitDB.bat
+++
@@ -1,5 +0,0 @@
-@REM This is a correct comment for .bat files
-docker compose -f library.yml run -it --rm db-backup sh -c "exec /custom_scripts/createDump.sh"
-docker compose -f library.yml down db
-docker volume rm library_mariadb-data-dir
-docker compose -f library.yml up -d db
