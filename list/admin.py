bb22e087d1be6053fc25da4bbaf1ea995f822c67
mia83 <79408337+mia83@users.noreply.github.com>
2023-11-27 23:13:54 +0100

    LIB-65 Displayed author and readdate for model books in admin

--- list/admin.py
+++ list/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import books, genres, grades, authors
 
-admin.site.register(books)
+#admin.site.register(books)
 admin.site.register(genres)
 admin.site.register(grades)
 admin.site.register(authors)
+
+@admin.register(books)
+class BooksAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'readdate')
