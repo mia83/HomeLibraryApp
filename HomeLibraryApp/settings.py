f22c61608c1f0a8fc1b0fc3c66eea0fd277334cb
mia83 <79408337+mia83@users.noreply.github.com>
2023-11-27 15:35:27 +0100

    LIB-74 ALLOWED_HOSTS in correct format with default value

--- HomeLibraryApp/settings.py
+++ HomeLibraryApp/settings.py
@@ -38,11 +38,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = env.list(
-    'ALLOWED_HOSTS',
-    cast=[str],
-    default=[]
-)
+ALLOWED_HOSTS = [x for x in env.list('ALLOWED_HOSTS', default=[])]
 
 
 # Application definition
